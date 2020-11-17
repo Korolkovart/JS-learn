0d1e7a2f4c12e35d707fff0a3fa86e7060591683
Korolkovart <korolkovar@yandex.ru>
2020-11-18 01:45:18 +0300

    fixed, hw lesson22, please check

--- todo/js/script.js
+++ todo/js/script.js
@@ -17,10 +17,6 @@ class Todo {
       this.todoList.textContent = '';
       this.todoCompleted.textContent = '';
       this.todoData.forEach(this.createItem, this)
-      // this.todoData.forEach(this.completedItem, this)
-      // this.todoData.forEach(this.handler, this)
-
-
       this.addToStorage();
   }
 
@@ -31,8 +27,8 @@ class Todo {
     li.insertAdjacentHTML('beforeend', `
         <span class="text-todo">${todo.value}</span>
         <div class="todo-buttons">
-          <button class="todo-remove"></button>
-          <button class="todo-complete"></button>
+          <button class="todo-remove" data-id="${todo.key}"></button>
+          <button class="todo-complete" data-id="${todo.key}"></button>
         <div>
     `);
 
@@ -41,12 +37,6 @@ class Todo {
     } else {
       this.todoList.append(li)
     }
-
-  // const todoComplete = li.querySelector('.todo-complete')
-  // todoComplete.addEventListener('click', () => {
-  //   console.log(todo);
-  // })
-
   }
 
 
@@ -64,52 +54,40 @@ class Todo {
     }
   }
 
-  generateKey(){
+  generateKey() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
   }
-  deleteItem(){
-    // this.todoData.forEach((item) => {
-    //   const index = this.todoData.findIndex(el => el.value === item.value);
-    //   if (index !== -1) {
-    //     this.todoData.splice(index, 1);
-    //   }
-    //   render()
-    // })
-   
-    // console.log([...this.todoData]);
-    // [...this.todoData].forEach((elem, item, ) => {
-    //   console.log(elem[1].completed);
-    //   elem[1].completed = !elem[1].completed
-    //   console.log(item);
-
-    //   this.render()
-    // })
+  deleteItem = (id) => {
+    this.todoData.forEach((todo) => {
+      if(todo.key === id){
+        this.todoData.delete(id);
+      }
+      this.render()
+    })
   }
-
-  completedItem(){
-    this.todoData.forEach((todo, item) => {
-      console.log(todo);
-      console.log(item);
-          
-      todo.completed = !todo.completed
+  completedItem = (id) => {
+    this.todoData.forEach((todo) => {
+      if(todo.key === id){
+        todo.completed = !todo.completed
+      }
     })
     this.render()
+
   }
 
-  handler(){
+  handler(){    
     const todoContainer = document.querySelector('.todo-container');
     todoContainer.addEventListener('click', (event) => {
+
       let target = event.target;
-      if(target.classList.contains('todo-remove')){
-        this.deleteItem()
+      let id = event.target.dataset.id
+
+      if(target.closest('.todo-remove')){
+        this.deleteItem(id)
       } 
 
       if(target.closest('.todo-complete')){
-        // console.log('completed target: ', target);
-        // console.log('completed elem: ', elem);
-        // console.log('completed todo: ', todo);
-    
-        this.completedItem()
+        this.completedItem(id)
       }
 
     })
@@ -118,12 +96,9 @@ class Todo {
   init(){
     this.form.addEventListener('submit', this.addTodo.bind(this))
     this.render()
-    // this.handler()
   }
 }
 
 const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed')
 todo.init()
 todo.handler()
-
-// todo.deleteItem()
